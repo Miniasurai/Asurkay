ed8c9225ce0e242dce74d7bd0103b38a3acc69ac
Miniasurai <115645368+Miniasurai@users.noreply.github.com>
2022-12-08 04:16:37 +0700

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const Asurkay = require("./structures/Client");
-const client = new Asurkay();
-const { Collection } = require("discord.js");
-const blacklistedWords = new Collection();
-module.exports = { blacklistedWords };
-client.connect()
-module.exports = client;
